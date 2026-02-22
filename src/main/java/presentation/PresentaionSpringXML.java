03ff2f026dcee9ae59363f942723bf35c8985834
YassineBahadi <yassinebahadi04@gmail.com>
2026-02-22 16:03:19 +0000

    feat:implement dependency injection using spring xml configuration(config.xml)

---
+++ src/main/java/presentation/PresentaionSpringXML.java
@@ -0,0 +1,17 @@
+package presentation;
+
+import metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author pc
+ **/
+public class PresentaionSpringXML {
+    public static  void main(String[] args){
+        ApplicationContext springContext=new ClassPathXmlApplicationContext("config.xml");
+        IMetier metier=(IMetier) springContext.getBean("metier");
+//        IMetier metier=springContext.getBean(IMetier.class);
+        System.out.println("Résult="+metier.calcul());
+    }
+}
